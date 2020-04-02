8208ae8c0c4d78884a506794dff264d86e1ac87c
syahaina20 <syahaina20@gmail.com>
2020-04-02 13:42:54 +0800

    Updates

---
+++ precache-manifest.29d8654d6768ab4e3946a5527e899b9d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614fc76d3bd55bf756b78f11e88481a9",
+    "url": "/React-in-github-weather/index.html"
+  },
+  {
+    "revision": "e1c7aa0ce6f3cdd8fc94",
+    "url": "/React-in-github-weather/static/css/2.34f0f139.chunk.css"
+  },
+  {
+    "revision": "e9a15915ccce44d37278",
+    "url": "/React-in-github-weather/static/css/main.7d393ff3.chunk.css"
+  },
+  {
+    "revision": "e1c7aa0ce6f3cdd8fc94",
+    "url": "/React-in-github-weather/static/js/2.3bd3a2a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-in-github-weather/static/js/2.3bd3a2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a15915ccce44d37278",
+    "url": "/React-in-github-weather/static/js/main.800b33bc.chunk.js"
+  },
+  {
+    "revision": "744ea261480ff013e2aa",
+    "url": "/React-in-github-weather/static/js/runtime-main.3387a115.js"
+  },
+  {
+    "revision": "ab11d1a650da661e623a24d13d6e03cb",
+    "url": "/React-in-github-weather/static/media/bg.ab11d1a6.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/React-in-github-weather/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/React-in-github-weather/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/React-in-github-weather/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/React-in-github-weather/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/React-in-github-weather/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
